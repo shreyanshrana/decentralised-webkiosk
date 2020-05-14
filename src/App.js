c69ec9008b987631c4bb389c1d69d2e68a2e35de
manmeetkc <61089525+manmeetkc@users.noreply.github.com>
2020-05-14 23:29:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+
 import React from "react";
 import { Button } from "semantic-ui-react";
 import Routes from "./routes";
+import Info from "./Components/Info/info.js";
 
 function App() {
+  const manmeet = {};
   return (
     <div className="App">
       <Routes />
+      <Info
+        name="Manmeet"
+        rollno="101703329"
+        email="mkcshs@gmail.com"
+        branch="Computer Science"
+        gradyear="2021"
+      />
     </div>
   );
 }
